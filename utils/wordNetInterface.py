fba085b63cc577f74d293894aac502aa714dc8c9
Nicholas Parslow <nparslow@yahoo.com.au>
2015-09-29 12:55:56 +0200

    add a python file for wordnet interface functions

---
+++ utils/wordNetInterface.py
@@ -0,0 +1,9 @@
+__author__ = 'nparslow'
+
+# goal is to be able to look up synonyms, hypernyms etc. of a given word (or lemma probably)
+
+def test():
+    pass
+
+if __name__ == "__main__":
+    test()
