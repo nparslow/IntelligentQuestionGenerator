42b2e98c1ef7d376ee7e43542207e465256ee909
Nicholas Parslow <nparslow@yahoo.com.au>
2015-09-29 13:35:03 +0200

    add a python file for wordnet interface functions

--- utils/wordNetInterface.py
+++ utils/wordNetInterface.py
@@ -1,6 +1,10 @@
 __author__ = 'nparslow'
 
 # goal is to be able to look up synonyms, hypernyms etc. of a given word (or lemma probably)
+# can we use nltk with the french wordnet file? oh actually nltk has french wordnet!
+# looks like no non-english lemmatiser in ntlk need to use lefff based lematiser so sxpipe?
+
+WORDNET_FILE = "/home/nparslow/Documents/WOLF/wolf-1.0b4.xml"
 
 def test():
     pass
